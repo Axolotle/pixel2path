dc25c3ff34f2fe803503dbc6d4cda1638605ffba
Axolotle <nchesnais@mailoo.org>
2018-01-02 19:05:06 +0100

    adding new method to define path directions from a string instead of an image

--- px2svg.py
+++ px2svg.py
@@ -1,4 +1,5 @@
 import cv2
+from json import load
 from svgwrite import Drawing
 from svgwrite.container import Group
 from svgwrite.path import Path
@@ -8,6 +9,7 @@ class Px2path:
     def __init__(self, filename, w=2.25):
         self.w = w
         self.paths = []
+        self.filename = filename.split('.')[0]
 
         if filename.find('json') > -1:
             self.points = self.from_json(filename)
@@ -32,26 +34,33 @@ class Px2path:
         # Remove the intensity information
         points = [p[0] for p in points]
 
-        # Adjust the position from the given stroke_width
-        # Also round it if there's no decimals
-        for i, point in enumerate(points):
-            position = [p * self.w for p in point]
-            points[i] = [round(p) if round(p) == p else p for p in position]
+        return points
+
+    def from_json(self, filename):
+        with open(filename, 'r') as f:
+            elems = load(f)
+
+        points = [[int(e)  for e in elem.split(',')]
+                  for elem in elems['0'].split(' ')]
 
         return points
 
     def to_line(self, x=0):
         # Generate a path line from given points positions
+        def pos(string, x=0):
+            return string.format(a * self.w + self.w / 2 + x,
+                                 b * self.w + self.w / 2)
+
         a, b = self.points.pop(0)
-        d = 'M{},{} L'.format(a + x, b)
+        d = pos('M{},{} L')
         for p in self.points:
             a, b = p
-            d += '{},{} '.format(a + x, b)
-            
+            d += pos('{},{} ')
+
         self.paths.append(Path(d=d + 'Z'))
 
-    def generate_file(self, filename):
-        doc = Drawing(filename + '.svg', profile='tiny',
+    def generate_file(self):
+        doc = Drawing(self.filename + '.svg', profile='tiny',
                       size=('200px', '200px'), viewBox='0 0 200 200')
         main = Group(fill='none', stroke='black', stroke_linejoin='round',
                      stroke_width=str(self.w) + 'px', stroke_linecap='round')
@@ -62,6 +71,6 @@ class Px2path:
         doc.save(pretty=True)
 
 
-drawing = Px2path('0.png')
+drawing = Px2path('0.json')
 drawing.to_line()
-drawing.generate_file('path')
+drawing.generate_file()
