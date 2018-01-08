acc814cb8cb239dea24b6d7eea021f47ba11510b
Axolotle <nchesnais@mailoo.org>
2018-01-08 18:40:58 +0100

    Add option to generate inner grid

--- grid_gen.py
+++ grid_gen.py
@@ -1,29 +1,33 @@
 from cv2 import imwrite as save_as
-from numpy import zeros, uint8
+from numpy import ones, uint8
 
-def generate_img_grid(filename, number, grid, color, bg_color=[255] * 3):
+def generate_img_grid(fp, n, grid, color, bg_color, inner_grid=[1,1], alt_color=None):
     """ Generate an image with a grid to draw characters """
     row = grid[1] + 2
-    col = (grid[0] + 1) * number + 1
-    bg_color = list(reversed(bg))
+    col = (grid[0] + 1) * n + 1
+    bg_color = list(reversed(bg_color))
     color = list(reversed(color))
+    alt_color = list(reversed(alt_color)) if alt_color is not None else None
 
-    img = zeros((row, col, 3), uint8)
-    img[:,:] = color
-    img[0,:] = bg
-    img[row - 1,:] = bg
+    img = ones((row, col, 3), uint8) * 255
 
-    for i in range(col):
-        if i % (grid[0] + 1) == 0:
-            img[:,i] = bg
+    for a in range(1, n*(grid[0]+1), grid[0]+1):
+        img[1:row - 1, a:a + grid[0]] = color
+        if alt_color is not None:
+            marg_y = 1 + inner_grid[1]
+            marg_x = inner_grid[0]
+            img[marg_y:row - marg_y, a + marg_x:a + grid[0] - marg_x] = alt_color
 
-    save_as(filename, img)
+    save_as(fp, img)
 
 options = {
-    'filename': 'PxPath.png',
-    'number': 249,
+    'fp': 'PxPath[5,9].png',
+    'n': 10,
     'grid': [5, 9],
+    'inner_grid': [1, 2],
     'color': [255, 0, 0],
+    'alt_color' : [255, 50, 50],
+    'bg_color': [255, 255, 255]
 }
 
 generate_img_grid(**options)
