b4fec43fef386f905c0cc28de262bb3179352b26
Axolotle <nchesnais@mailoo.org>
2018-01-18 13:55:35 +0100

    renaming and splitting files

--- px2ph.py
+++ px2ph.py
@@ -1,66 +1,12 @@
-from os import listdir
-from os.path import isfile, join
-
-import cv2
 from svgwrite import Drawing
 from svgwrite.container import Group
 from svgwrite.path import Path
-from svgwrite.shapes import Rect
-from svgwrite.base import BaseElement
 
 from base_objects import Point, Line
+from px2pt import Px2Pt
 
 
-class Px2coord:
-    def __init__(self, filepath, characters, grid):
-        self.x, self.y = grid
-
-        if filepath[-1] == '/':
-            self.filename = filepath.strip('/')
-            self.characters = self.from_img(filepath, characters)
-
-    def from_img(self, filepath, characters):
-        """
-        Read several images and parse pixel position in absolute position
-        for each characters given as argument. points's lists are ordered
-        by grey intensity.
-        """
-        layers = [f for f in listdir(filepath) if isfile(join(filepath, f))]
-        char_str = list(characters)
-        # generate a dict with all characters given
-        characters = {x: {'closed': False, 'points': []}
-                      for x in char_str}
-
-        for layer in layers:
-            # Read & convert the image to greyscale
-            img = cv2.imread(filepath + layer)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            # Extract every characters from the image
-            rects = [img[1:self.y + 1, a:a + self.x]
-                     for a in range(1, len(char_str)*(self.x + 1), self.x + 1)]
-
-            for char, rect in zip(char_str, rects):
-                # get the pixel position (x,y) and intensity if it's not white
-                points = [[Point(x, y), rect[y, x]]
-                          for x in range(self.x)
-                          for y in range(self.y)
-                          if rect[y, x] < 255]
-
-                if len(points) == 0:
-                    continue
-                # Sort the list by intensity
-                points = sorted(points, key=lambda info: info[1])
-                # If the light intensity of the first point is 0 (black)
-                # then the character's path has to be closed
-                if points[0][1] == 0:
-                    characters[char]['closed'] = True
-                # Remove the intensity information
-                points = [p[0] for p in points]
-                characters[char]['points'].append(points)
-
-        return characters
-
-class Px2path(Px2coord):
+class Px2Ph(Px2Pt):
     def __init__(self, filepath, characters, grid, w=2.25):
         self.w = w
         super().__init__(filepath, characters, grid)
@@ -102,19 +48,11 @@ class Px2path(Px2coord):
         return Path(d=d)
 
     def rel_path(self, char, dx=0):
-        def mult(vector):
-            decal = self.w / 2
-            return Vector(vector[0] * decal, vector[1] * decal)
-        def mult2(vector):
-            return Vector(vector[0] * self.w, vector[1] * self.w)
-
         char = self.characters[char]
 
         paths = list()
 
         for l, layer in enumerate(char['points']):
-            # if l != 6:
-            #     continue
             outer = list()
             inner = list()
             for i, phase in enumerate(layer):
@@ -201,16 +139,6 @@ class Px2path(Px2coord):
                      for dx, char in enumerate(list(string))]
 
         self.generate_file(paths)
-
-    def draw_grid(self, max_x, max_y):
-        group = Group(fill='grey')
-        rects = [Rect(insert=(x * self.w, y * self.w), size=(self.w, self.w))
-                 for x in range(max_x) for y in range(max_y)]
-        for i, rect in enumerate(rects):
-            if i % 2 == 0:
-                rect['fill'] = "#AAAAAA"
-            group.add(rect)
-        return group
 
     def generate_file(self, paths):
         x = self.w * (self.x * len(paths) + 1)
---
+++ px2pt.py
@@ -0,0 +1,54 @@
+import cv2
+from os import listdir
+from os.path import isfile, join
+
+from base_objects import Point
+
+
+class Px2Pt:
+    def __init__(self, filepath, characters, grid):
+        self.x, self.y = grid
+
+        if filepath[-1] == '/':
+            self.filename = filepath.strip('/')
+            self.characters = self.from_img(filepath, characters)
+
+    def from_img(self, filepath, characters):
+        """
+        Read several images and parse pixel position in absolute position
+        for each characters given as argument. points's lists are ordered
+        by grey intensity.
+        """
+        layers = [f for f in listdir(filepath) if isfile(join(filepath, f))]
+        char_str = list(characters)
+        # generate a dict with all characters given
+        characters = {x: {'closed': False, 'points': []} for x in char_str}
+
+        for layer in layers:
+            # Read & convert the image to greyscale
+            img = cv2.imread(filepath + layer)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Extract every characters from the image
+            rects = [img[1:self.y + 1, a:a + self.x]
+                     for a in range(1, len(char_str)*(self.x + 1), self.x + 1)]
+
+            for char, rect in zip(char_str, rects):
+                # get the pixel position (x,y) and intensity if it's not white
+                points = [[Point(x, y), rect[y, x]]
+                          for x in range(self.x)
+                          for y in range(self.y)
+                          if rect[y, x] < 255]
+
+                if len(points) == 0:
+                    continue
+                # Sort the list by intensity
+                points = sorted(points, key=lambda info: info[1])
+                # If the light intensity of the first point is 0 (black)
+                # then the character's path has to be closed
+                if points[0][1] == 0:
+                    characters[char]['closed'] = True
+                # Remove the intensity information
+                points = [p[0] for p in points]
+                characters[char]['points'].append(points)
+
+        return characters
