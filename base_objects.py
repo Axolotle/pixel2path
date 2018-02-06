c086ebdb7fbbd8139f84d39e9a7ebddaff2e46cc
Axolotle <nchesnais@mailoo.org>
2018-02-06 11:54:10 +0100

    Adding Shape's stroke vectorization

--- base_objects.py
+++ base_objects.py
@@ -105,6 +105,10 @@ class Stroke:
             self.layers = self.points_position(relative)
 
     def format(self, layers):
+        """ Format points position to a list of tuples composed of the point's
+        coordinates and the segment's type. If the stroke is not closed,
+        the first point is a 'move' segment.
+        """
         glyph_pts = list()
         for path in layers:
             path_pts = list()
@@ -115,16 +119,74 @@ class Stroke:
         return glyph_pts
 
     def points_position(self, relative=False):
+        """ Change points to relative or absolute position
+        """
         if self.relative is relative:
             pass
         glyph_pts = list()
-        for path in self.layers:
-            new_points = [path[0]]
+        for pts in self.layers:
+            new_pts = [pts[0]]
             for i in range(1, len(path)):
+                # rebuild the tuple by changing points position
                 if relative:
-                    new_points.append((path[i][0] - path[i-1][0], path[i][1]))
+                    new_pts.append((pts[i][0] - pts[i-1][0], pts[i][1]))
                 else:
-                    new_points.append((new_points[i-1][0] + path[i][0], path[i][1]))
-            glyph_pts.append(new_points)
+                    new_pts.append((new_pts[i-1][0] + pts[i][0], pts[i][1]))
+            glyph_pts.append(new_pts)
         self.relative = relative
         return glyph_pts
+
+    # TODO: apply oblique style to points
+
+class Shape(Stroke):
+    def __init__(self, layers, width, linejoin='round', linecap='round', angle=None):
+        super().__init__(layers, relative=False)
+        decal = width / 20
+        self.layers = self.vectorize(decal, linejoin, linecap)
+
+    def vectorize(self, decal, linejoin, linecap):
+        new_layers = list()
+        for layer in self.layers:
+            # TODO check direction of stroke to define theta_in and theta_out
+            closed = layer[0][1] != 'move'
+            length = len(layer)
+            starter = 0
+            outer, inner = list(), list()
+            new_layer = list()
+            points = [path[0] for path in layer]
+
+            if not closed:
+                a, b = points[0], points[1]
+                outer += [(a.parallel(b, -90, decal), 'line')]
+                inner += [(a.parallel(b, 180, decal), 'curve'),
+                          (a.parallel(b, 90, decal), 'curve')]
+                starter = 1
+
+            for i in range(starter, length - 1):
+                z, a, b = points[i-1], points[i], points[i+1]
+                outer += [self.get_intersection(z, a, b, -90)]
+                inner += [self.get_intersection(z, a, b, 90)]
+
+            if closed:
+                z, a, b = points[length-2], points[length-1], points[0]
+                outer += [self.get_intersection(z, a, b, -90)]
+                inner += [self.get_intersection(z, a, b, 90)]
+                new_layer += [outer, list(reversed(inner))]
+
+            else:
+                a, b = points[length-1], points[length-2]
+                inner += [(a.parallel(b, -90, decal), 'line'),
+                          (a.parallel(b, 180, decal), 'curve')]
+                outer += [(a.parallel(b, 90, decal), 'curve')]
+                new_layer.append(outer + list(reversed(inner)))
+
+            new_layers.append(new_layer)
+
+        return new_layers
+
+    def get_intersection(self, a, b, c, theta):
+        d1, d2 = Line(a, b).parallel(theta, 0.5), Line(b, c).parallel(theta, 0.5)
+        intersection = d1.intersection(d2)
+        if intersection is None:
+            return Line(d1.b, d2.a)
+        return intersection
