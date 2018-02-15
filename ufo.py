a257ba211b49e1eb435a85e0fcc3cdd3b1af84b6
Axolotle <nchesnais@mailoo.org>
2018-02-15 01:04:09 +0100

    Updating ufo generation

--- ufo.py
+++ ufo.py
@@ -1,22 +1,9 @@
-from base_objects import Shape
-from defcon import Font, Glyph, Contour, Point
+from base_objects import Stroke
+from defcon import Font
 
-def gen_font(glyphs, stroke_style):
+def gen_ufo(glyphs, stroke_style):
     font = Font()
     for glyph_name in glyphs:
-        shape = Shape(glyphs[glyph_name], **stroke_style)
-        font_glyph = Glyph()
-        for layer in shape.layers:
-            for contour in layer:
-                new_contour = Contour()
-                for point in contour:
-                    # print(point)
-                    # continue
-                    new_contour.addPoint(point[0], point[1])
-                    # print(new_contour[0].x)
-                font_glyph.appendContour(new_contour)
-            font.insertGlyph(font_glyph, glyph_name)
-
-
-    print(font.unicodeData.forcedUnicodeForGlyphName('A'))
+        font_glyph = Stroke(glyphs[glyph_name]).scale(5).vectorize(**stroke_style)
+        font.insertGlyph(font_glyph, glyph_name)
     font.save(path='../pxph.ufo')
