cadf5fb4290fedc4c8d64812060aac1cebc64187
Axolotle <nchesnais@mailoo.org>
2020-02-21 14:35:47 +0100

    add yaml dump helper

--- px2ph/utils/yaml.py
+++ px2ph/utils/yaml.py
@@ -1,5 +1,9 @@
-from yaml import load, CLoader as Loader
+from yaml import load, CLoader as Loader, dump, CDumper as Dumper
 
 def get_yaml(fp):
     with open(fp, 'r') as yaml_file:
         return load(yaml_file.read(), Loader=Loader)
+
+def save_as_yaml(fp, data):
+    with open(fp, 'w') as yaml_file:
+        yaml_file.write(dump(data, Dumper=Dumper))
